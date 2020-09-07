0da2cfef49b20f1d701b305d80c773c09167b38f
sankalp <48873086+sankalp-tr@users.noreply.github.com>
2020-09-07 17:36:14 +0530

    Create userReducer.js

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,10 @@
+function userReducer(state = [], action) {
+	switch(action.type) {
+		case "CREATE_USER" :
+			return [...state, {...action.user}];
+		default :
+			return state;
+	}
+}
+
+export default userReducer;
